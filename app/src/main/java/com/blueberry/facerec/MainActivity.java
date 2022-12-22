bbdf94efe570d278712d9d41951175eb6d475df5
muyonggang <muyonggang@bytedance.com>
2022-12-22 08:49:53 +0800

    feat: select image

--- app/src/main/java/com/blueberry/facerec/MainActivity.java
+++ app/src/main/java/com/blueberry/facerec/MainActivity.java
@@ -2,8 +2,10 @@ package com.blueberry.facerec;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -19,11 +21,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Arrays;
+
 // http://www.milbo.org/stasm-files/stasm4.pdf
 public class MainActivity extends Activity {
 
     private static final String TAG = "MainActivity";
 
+    private static final int SELECT_IMAGE = 200;
     private String xmlPath;
 
     private String fileName = "lbpcascade_frontalface.xml";
@@ -55,32 +59,41 @@ public class MainActivity extends Activity {
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission
                     .WRITE_EXTERNAL_STORAGE}, 100);
-        } else {
-            new Thread(new WritePictureRunnable()).start();
         }
+    }
+
+
+    public void selectImage(int requestCode) {
+        final String permission = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+        if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission
+                    .WRITE_EXTERNAL_STORAGE}, 100);
+            return;
+        }
+
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, requestCode);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if (requestCode == 100) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                new Thread(new WritePictureRunnable()).start();
-            }
-        }
     }
 
-    class MyOnClickListener implements View.OnClickListener {
-        @Override
-        public void onClick(final View v) {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SELECT_IMAGE) {
+            Uri uri = data.getData();
+            String path = MediaStoreUtils.getPathFromUri(this, uri);
             new Thread() {
                 @Override
                 public void run() {
                     final Face[] faces = new FaceDetector(xmlPath + "/" + fileName)
-                            .detect(picPath);
+                            .detect(path);
                     Log.d(TAG, "run: detect:" + Arrays.toString(faces));
-                    v.post(new Runnable() {
+                    tv.post(new Runnable() {
                         @Override
                         public void run() {
                             tv.setText(Arrays.toString(faces));
@@ -91,63 +104,42 @@ public class MainActivity extends Activity {
         }
     }
 
-    class WritePictureRunnable implements Runnable {
+class MyOnClickListener implements View.OnClickListener {
+    @Override
+    public void onClick(final View v) {
+        selectImage(SELECT_IMAGE);
+    }
+}
 
-        @Override
-        public void run() {
-            writePic();
-        }
 
-        private void writePic() {
-            File dstFile = new File(picPath);
-            AssetManager assetManager = getAssets();
-            InputStream inputStream = null;
-            OutputStream out = null;
-            try {
-                inputStream = assetManager.open("girls.jpg");
-                out = new FileOutputStream(dstFile);
-                int len = -1;
-                byte[] bytes = new byte[8096];
-                while ((len = inputStream.read(bytes)) != -1) {
-                    out.write(bytes, 0, len);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                FileUtil.close(inputStream);
-                FileUtil.close(out);
-            }
-        }
+
+class WriteXmlRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        writeXml();
     }
 
-    class WriteXmlRunnable implements Runnable {
-
-        @Override
-        public void run() {
-            writeXml();
-
-        }
-
-        private void writeXml() {
-            File dstFile = new File(xmlPath, fileName);
-            AssetManager assetManager = getAssets();
-            InputStream inputStream = null;
-            OutputStream out = null;
-            try {
-                inputStream = assetManager.open(fileName);
-                out = new FileOutputStream(dstFile);
-                int len = -1;
-                byte[] bytes = new byte[8096];
-                while ((len = inputStream.read(bytes)) != -1) {
-                    out.write(bytes, 0, len);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                FileUtil.close(inputStream);
-                FileUtil.close(out);
+    private void writeXml() {
+        File dstFile = new File(xmlPath, fileName);
+        AssetManager assetManager = getAssets();
+        InputStream inputStream = null;
+        OutputStream out = null;
+        try {
+            inputStream = assetManager.open(fileName);
+            out = new FileOutputStream(dstFile);
+            int len = -1;
+            byte[] bytes = new byte[8096];
+            while ((len = inputStream.read(bytes)) != -1) {
+                out.write(bytes, 0, len);
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            FileUtil.close(inputStream);
+            FileUtil.close(out);
         }
     }
+}
 
 }
